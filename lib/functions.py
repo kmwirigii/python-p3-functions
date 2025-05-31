5ec88cf7f510e7ab8cc7c1a0fd0acfaa9859c077
skiees <kmwirigi05@gmail.com>
2025-05-31 09:50:15 +0300

    adds message

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print(f'Hello, programmer!')
     pass
 
-def greet(name):
+def greet(name='Guido'):
+    print(f'Hello, {name}!')
     pass
 
 def greet_with_default(name="programmer"):
+    print(f'Hello, {name}!')
     pass
 
-def add(num1, num2):
+def add(num1=45,num2= 55):
+    return num1+num2
     pass
 
-def halve(number):
+def halve(number=6):
+    return number/2
     pass
+def greet_with_default_with_param(name='Guido'):
+    print(f"Hello, {name}")
